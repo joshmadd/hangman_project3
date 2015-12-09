adb468e96c015045cc0feecd37d5969be4169d75
Joshua Madden <jmadden@localhost.localdomain>
2015-12-08 19:27:03 -0500

    Final update (hopefully)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # hangman_project3
-This is my third project that creates a game of hangman for the user to play given a preset list of words for the game to choose from.
+This is my third project that creates a game of hangman for the user to play
+given a preset list of words for the game to choose from.
+
+In order to play the game, simply download the file to your computer that can run python.
+Execute the file and read the welcome messages.
+After reading the messages, the user should enter a letter they think is in the word
+that is represented by the asterixes.
+
+The user then has 6 chances to mess up before they either win or lose.
+Either case results in the end of the game.
+When the game is over the user has the opportunity to play again or quit out of the game.
--- hangman.py
+++ hangman.py
@@ -2,10 +2,14 @@ import random
 import sys
 import os
 
+# creates the list of words to choose from
 word_list = ["APPLE", "BANANA", "BLUEBERRY", "CHERRY", "GRAPE",
 	"GRAPEFRUIT", "KIWI", "LEMON", "LIME", "MANGO",
 	"NECTARINE", "ORANGE", "PEACH", "PINEAPPLE", "STRAWBERRY"]
 
+# uses the secret word to create the display word
+# it does this by setting the contents of the secret word
+# to a new list and changing all the contents to *
 def create_disp_word(word):
 	length = len(word)
 	disp_word = list(word)
@@ -15,6 +19,9 @@ def create_disp_word(word):
 		i = i + 1
 	return disp_word;
 
+# checks to see if the entered letter is in the secret word
+# and updates the displayed word if the entered letter is there
+# otherwise, it makes no changes
 def check_sec_word(secret, display, letter):
 	i = 0
 	while(i < len(secret)):
@@ -23,6 +30,8 @@ def check_sec_word(secret, display, letter):
 		i = i + 1
 	return display
 
+# checks to see if the lists entered as parameters to the
+# function are equal.  returns 0 if they are and 1 if they aren't
 def first_isnt_sec(first, second):
 	i = 0
 	while(i < len(first)):
@@ -35,32 +44,45 @@ def first_isnt_sec(first, second):
 play = 'Y'
 
 while(play[0] == 'Y'):
+	# picks a random number to be used in selecting a random word
+	# and sets values to begin and maintain the game
 	rand_num = random.randrange(0, 15)
 	turns = 6
-	
 	sec_word = list(word_list[rand_num])
 	disp_word = create_disp_word(sec_word)
 	
-	print(''.join(sec_word))
+	print("\n")
 	print(''.join(disp_word))
 	
+	# welcome message to the game
 	print("\nWelcome to the game of Hangman!")
 	print("Please enter a letter to begin guessing")
 	print("the word displayed above to try and save")
 	print("your man from the gallows.  Good luck!")
 	
+	# the game runs until the user wins by guessing all the letters
+	# or until the user loses by running out of turns
 	while((turns > 0) and (first_isnt_sec(sec_word, disp_word))):
+		
+		# gets a letter from the user
 		letter = sys.stdin.readline().upper()
+		print("\n")
 		temp_word = list(disp_word)	
 		disp_word = check_sec_word(sec_word, disp_word, letter)
-		print("\n")
+		
+		# checks to see if the letter entered was in the secret word and then
+		# displays the appropriate message based on the letter's existence
 		if(not first_isnt_sec(temp_word, disp_word)):
 			turns = turns - 1
-			if(turns > 0):
-				print("Oh, no! Your letter wasn't in the word!")
-				print "Try again you only have", turns, "turns left!\n"
+			print("Oh, no! Your letter wasn't in the word!")
+			if(turns > 1):
+				print "Try again, you only have", turns, "mistakes left!"
+			elif(turns == 1):
+				print "Try again, you only have", turns, "mistake left!"
 		elif(first_isnt_sec(sec_word, disp_word) and turns > 0):
-			print("Good job!  Now try for the other letters.\n")
+			print("Good job!  Now try for the other letters.")
+		
+		# the visual display of the hangman based on how many turns are left
 		print("_______")
 		print("      |")
 		print("      |")
@@ -97,6 +119,9 @@ while(play[0] == 'Y'):
 			print("  0       0")
 		print("\n")
 		print(''.join(disp_word))
+	
+	# gives output based on if the user won or lost
+	# and asks if they want to play again
 	if(turns == 0):
 		print("Sorry, you lost your man to the gallows!")
 	else:
@@ -104,4 +129,4 @@ while(play[0] == 'Y'):
 	print("Well, that's game over.  Would you like to try and save someone else? (Y/N)")
 	play = sys.stdin.readline().upper()
 
-print("Come play again!\n")
+print("\nCome play again!\n")
